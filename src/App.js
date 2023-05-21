9ff8815e293a1c24b63e47bc946d8cc24865c1d4
KNR1997 <kethakaranasinghe@gmail.com>
2023-05-21 23:05:57 +0530

    EC : useNavigate added to react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
 import './App.scss';
 // react router v6
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 // pages
-import {Home, CategoryProduct, ProductSingle, Cart, Search} from "./pages/index";
+import { Home, CategoryProduct, ProductSingle, Cart, Search } from "./pages/index";
 // components
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Footer from "./components/Footer/Footer";
 import store from "./store/store";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
+import { oktaConfig } from "./utils/oktaConfig"
+import { OktaAuth } from "@okta/okta-auth-js";
+import { Security, LoginCallback} from '@okta/okta-react';
+import LoginWidget from './components/Auth/LoginWidget';
+import {toRelativeUrl} from "@okta/okta-auth-js"
+
+const oktaAuth = new OktaAuth(oktaConfig);
 
 function App() {
+
+  const navigate = useNavigate();
+
+  const customAuthHandler = () => {
+    navigate('/login');
+  }
+
+  const restoreOriginalUri = async (_oktaAuth, originalUri) => {
+    navigate(toRelativeUrl(originalUri || '/', window.location.origin));
+  }
+
   return (
     <div className="App">
-      <Provider store = {store}>
-        <BrowserRouter>
-          <Header />
-          <Sidebar />
-
-          <Routes>
-            {/* home page route */}
-            <Route path = "/" element = {<Home />} />
-            {/* single product route */}
-            <Route path = "/product/:id" element = {<ProductSingle />} />
-            {/* category wise product listing route */}
-            <Route path = "/category/:category" element = {<CategoryProduct />} />
-            {/* cart */}
-            <Route path = "/cart" element = {<Cart />} />
-            {/* searched products */}
-            <Route path = "/search/:searchTerm" element = {<Search />} />
-          </Routes>
-
-          <Footer />
-        </BrowserRouter>
+      <Provider store={store}>
+          <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} onAuthRequired={customAuthHandler}>
+            <Header />
+            <Sidebar />
+            <Routes>
+              {/* home page route */}
+              <Route path="/" element={<Home />} />
+              {/* single product route */}
+              <Route path="/product/:id" element={<ProductSingle />} />
+              {/* category wise product listing route */}
+              <Route path="/category/:category" element={<CategoryProduct />} />
+              {/* cart */}
+              <Route path="/cart" element={<Cart />} />
+              {/* searched products */}
+              <Route path="/search/:searchTerm" element={<Search />} />
+              <Route path='/login'/>
+            </Routes>
+            <Footer />
+          </Security>
       </Provider>
     </div>
   );
--- src/components/Auth/LoginWidget.jsx
+++ src/components/Auth/LoginWidget.jsx
@@ -1,4 +1,4 @@
-import { Redirect } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useOktaAuth } from "@okta/okta-react";
 import Loader from "../Loader/Loader";
 import OktaSignInWidget from "./OktaSignInWidget";
@@ -20,7 +20,7 @@ const LoginWidget = ({ config }) => {
     }
 
     return authState.isAuthenticated ?
-    <Redirect to={{ pathname: '/'}}/>
+    <Navigate to={{ pathname: '/'}}/>
     :
     <OktaSignInWidget config={config} onSuccess={onSuccess} onError={onError}/>;
 };
