03d314969f0373cfca85ced58d9c5958b2ffa202
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2023-06-21 20:36:43 +0530

    privacy policy page

--- src/pages/PrivacyPolicy/PrivacyPolicyPage.js
+++ src/pages/PrivacyPolicy/PrivacyPolicyPage.js
@@ -48,30 +48,43 @@ const PrivacyPolicyPage = () => {
                   <p className='p1'>We do not, and will not, sell any of your personal data to any third party – including your name, address, email address or credit card information. We want to earn and maintain your trust, and we believe this is absolutely essential in order do that.</p>
                   <p className='p1'>However, we share your data with the following categories of companies as an essential part of being able to provide our services to you, as set out in this statement:<br></br></p>
                 
-                    <p className='p2'>
-                    &#x2022; Companies in the Pissu Kanna group, as sometimes different bits of our group are responsible for different activities (especially licensed ones)<br></br>
-                    &#x2022; Companies that do things to get your purchases to you, such as payment service providers, warehouses, order packers, and delivery companies<br></br>
-                    &#x2022; Professional service providers, such as marketing agencies, advertising partners and website hosts, who help us run our business<br></br>
-                    &#x2022; Credit reference agencies, law enforcement and fraud prevention agencies, so we can help tackle fraud<br></br>
-                    &#x2022; Companies approved by you, such as social media sites (if you choose to link your accounts to us).
-                    </p>
+                    <ul className='list'>
+                      <li>Companies in the Pissu Kanna group, as sometimes different bits of our group are responsible for different activities (especially licensed ones)</li>
+                      <li>Companies that do things to get your purchases to you, such as payment service providers, warehouses, order packers, and delivery companies</li>
+                      <li>Professional service providers, such as marketing agencies, advertising partners and website hosts, who help us run our business</li>
+                      <li>Credit reference agencies, law enforcement and fraud prevention agencies, so we can help tackle fraud</li>
+                      <li>Companies approved by you, such as social media sites (if you choose to link your accounts to us).</li> 
+                    </ul>
                     
                   <p className='p1'>We may provide third parties with aggregated but anonymised information and analytics about our customers and, before we do so, we will make sure that it does not identify you.</p>
 
                 <h3 className='tp1'>YOUR RIGHTS</h3>
                   <p className='p1'>You have a lot of rights relating to your personal information:</p>
                 
-                    <p className='p2'>
-                    &#x2022; The right to be informed about how your personal information is being used (like this notice!)<br></br>
-                    &#x2022; The right to access the personal information we hold about you<br></br>
-                    &#x2022; The right to request the correction of inaccurate personal information we hold about you <br></br>
-                    &#x2022; The right to request that we delete your data, or stop processing it or collecting it, in some circumstances<br></br>
-                    &#x2022; The right to stop direct marketing messages, and to withdraw consent for other consent-based processing at any time<br></br>
-                    &#x2022; The right to request that we transfer or port elements of your data either to you or another service provider<br></br>
-                    &#x2022; The right to complain to your data protection regulator <br></br>
-                    </p>
+                    <ul className='list'>
+                      <li>The right to be informed about how your personal information is being used (like this notice!)</li>
+                      <li>The right to access the personal information we hold about you</li>
+                      <li>The right to request the correction of inaccurate personal information we hold about you</li>
+                      <li>The right to request that we delete your data, or stop processing it or collecting it, in some circumstances</li>
+                      <li>The right to stop direct marketing messages, and to withdraw consent for other consent-based processing at any time</li>
+                      <li>The right to request that we transfer or port elements of your data either to you or another service provider</li>
+                      <li>The right to complain to your data protection regulator </li>
+                    </ul>
                     
                   <p className='p1'>We may provide third parties with aggregated but anonymised information and analytics about our customers and, before we do so, we will make sure that it does not identify you.</p>
+
+                  <h3 className='tp1'>HOW TO CONTACT US</h3>
+                    <p className='p1'>We always want to hear from our customers (especially if you feel we’ve let you down or could do better).</p>
+                    <p className='p1'>If you:</p>
+                  
+                      <ul className='list'>
+                        <li>Have any questions or feedback about this notice</li>
+                        <li>Would like us to stop using your information</li>
+                        <li>Want to exercise any of your rights as set out above, or have a complaint</li>
+                      </ul>
+                      
+                    <p className='p1'>please don’t hesitate to contact our Customer Care team, who will be happy to answer any questions you may have.</p>
+                    <p className='p1'>You can contact our privacy team by dropping us a line at <a href="mailto:kelanistore@gmail.com"><u>kelanistore@gmail.com </u></a></p>
               </div>
             </div>
           </div>
