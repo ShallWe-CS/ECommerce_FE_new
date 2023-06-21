ba0d655a682fcc4d8eb511cd3a293a87877b5aa5
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2023-06-21 15:16:26 +0530

    footer error

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className='footer bg-maroon'>
       <div className = "container py-4 text-center">
         <div className='flex align-center justify-center text-white fw-3 fs-14'>
-          <Link to = {'/PrivacyPolicyPage'} className='text-uppercase'>privacy policy</Link>
+          <Link to = {'/PrivacyPolicy'} className='text-uppercase'>privacy policy</Link>
           <div className='vert-line'></div>
           <Link to = "/" className='text-uppercase'>term of service</Link>
           <div className='vert-line'></div>
