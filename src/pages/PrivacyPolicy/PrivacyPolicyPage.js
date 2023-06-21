48e8e7e79f9655a872fd6408dd15714b5d350f96
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2023-06-21 20:14:23 +0530

    privacypage

--- src/pages/PrivacyPolicy/PrivacyPolicyPage.js
+++ src/pages/PrivacyPolicy/PrivacyPolicyPage.js
@@ -3,47 +3,75 @@ import './PrivacyPolicyPage.scss';
 const PrivacyPolicyPage = () => {
 
     return (
-      <div className='cat-products py-5 bg-white'>
+      <div className='cat-products bg-white'>
         <div className='container'>
           <div className='cat-products-content'>
 
             <div className='title-md'>
               <h2>PRIVACY POLICY</h2>
             </div>
-            <div className = 'privacycontent'>
+            
               <div className >
-                  <h3 className='my-3'></h3>
-                  <p>We’ll always keep your data safe and secure. So you’re clued up, here’s why we need it and how we use it.</p>
+                  <h3 className='my-1' ></h3>
+                  <p className='headcontent'>We’ll always keep your data safe and secure. So you’re clued up, here’s why we need it and how we use it.</p>
               </div>
+
+            <div className = 'privacycontent'>
 
               <div>
                 <img src='' alt=''/>
               </div>
 
               <div>
-                <h3 className='my-3'>1. THE GOOD STUFF</h3>
-                <p>We will only use your data to up your experience.</p>
-                <h3 className='my-3'>2. LOCKED DOWN</h3>
-                <p>We’ll protect your data like it’s our own.</p>
-                <h3 className='my-3'>3. STRAIGHT TALKING</h3>
-                <p>We’ll always talk your language and have your back – no-nonsense, no surprises.</p>
-                <h3 className='my-3'>4. NO SPAM</h3>
-                <p>You decide what and how you hear from us.</p>
-                <h3 className='my-3'>5. JUST THE ESSENTIALS</h3>
-                <p>Your info won’t just hang about – if we don’t need it, we’ll delete it.</p>
-                <h3 className='my-3'>PROTECTING YOUR PRIVACY</h3>
-                <p>Pissu Kanna, we are 100% committed to protecting the privacy and security of our customers and site visitors (yep, we mean you). The Pissu Kanna team members are customers themselves, of both Pissu Kanna and other internet sites – so we understand  how important your  privacy is. If you have any questions about how we Protect Your Privacy, drop us a line at hello@pissukanna.com
-                    For all our services, the data controller — the company that’s responsible for your privacy— is pissukanna.com </p>
-                <h3 className='my-3'>SHARING YOUR INFORMATION</h3>
-                <p>We do not, and will not, sell any of your personal data to any third party – including your name, address, email address or credit card information. We want to earn and maintain your trust, and we believe this is absolutely essential in order do that.
-                    However, we share your data with the following categories of companies as an essential part of being able to provide our services to you, as set out in this statement:
-                    Companies in the Pissu Kanna group, as sometimes different bits of our group are responsible for different activities (especially licensed ones)
-                    Companies that do things to get your purchases to you, such as payment service providers, warehouses, order packers, and delivery companies
-                    Professional service providers, such as marketing agencies, advertising partners and website hosts, who help us run our business
-                    Credit reference agencies, law enforcement and fraud prevention agencies, so we can help tackle fraud
-                    Companies approved by you, such as social media sites (if you choose to link your accounts to us).
-                    We may provide third parties with aggregated but anonymised information and analytics about our customers and, before we do so, we will make sure that it does not identify you.</p>
+                <h3 className='tp1'>1. THE GOOD STUFF</h3>
+                  <p className='p1'>We will only use your data to up your experience.</p>
 
+                <h3 className='tp1'>2. LOCKED DOWN</h3>
+                  <p className='p1'>We’ll protect your data like it’s our own.</p>
+
+                <h3 className='tp1'>3. STRAIGHT TALKING</h3>
+                  <p className='p1'>We’ll always talk your language and have your back – no-nonsense, no surprises.</p>
+
+                <h3 className='tp1'>4. NO SPAM</h3>
+                  <p className='p1'>You decide what and how you hear from us.</p>
+
+                <h3 className='tp1'>5. JUST THE ESSENTIALS</h3>
+                  <p className='p1'>Your info won’t just hang about – if we don’t need it, we’ll delete it.</p>
+
+                <h3 className='tp1'>PROTECTING YOUR PRIVACY</h3>
+                  <p className='p1'>KelaniStore, we are 100% committed to protecting the privacy and security of our customers and site visitors (yep, we mean you).
+                    The KelaniStore team members are customers themselves, of both KelaniStore and other internet sites – so we understand  how important your  privacy is.
+                    If you have any questions about how we Protect Your Privacy, drop us a line at kelanistore@gmail.com<br></br></p>
+                  <p className='p1'>For all our services, the data controller — the company that’s responsible for your privacy— is KelaniStore.com </p>
+
+                <h3 className='tp1'>SHARING YOUR INFORMATION</h3>
+                  <p className='p1'>We do not, and will not, sell any of your personal data to any third party – including your name, address, email address or credit card information. We want to earn and maintain your trust, and we believe this is absolutely essential in order do that.</p>
+                  <p className='p1'>However, we share your data with the following categories of companies as an essential part of being able to provide our services to you, as set out in this statement:<br></br></p>
+                
+                    <p className='p2'>
+                    &#x2022; Companies in the Pissu Kanna group, as sometimes different bits of our group are responsible for different activities (especially licensed ones)<br></br>
+                    &#x2022; Companies that do things to get your purchases to you, such as payment service providers, warehouses, order packers, and delivery companies<br></br>
+                    &#x2022; Professional service providers, such as marketing agencies, advertising partners and website hosts, who help us run our business<br></br>
+                    &#x2022; Credit reference agencies, law enforcement and fraud prevention agencies, so we can help tackle fraud<br></br>
+                    &#x2022; Companies approved by you, such as social media sites (if you choose to link your accounts to us).
+                    </p>
+                    
+                  <p className='p1'>We may provide third parties with aggregated but anonymised information and analytics about our customers and, before we do so, we will make sure that it does not identify you.</p>
+
+                <h3 className='tp1'>YOUR RIGHTS</h3>
+                  <p className='p1'>You have a lot of rights relating to your personal information:</p>
+                
+                    <p className='p2'>
+                    &#x2022; The right to be informed about how your personal information is being used (like this notice!)<br></br>
+                    &#x2022; The right to access the personal information we hold about you<br></br>
+                    &#x2022; The right to request the correction of inaccurate personal information we hold about you <br></br>
+                    &#x2022; The right to request that we delete your data, or stop processing it or collecting it, in some circumstances<br></br>
+                    &#x2022; The right to stop direct marketing messages, and to withdraw consent for other consent-based processing at any time<br></br>
+                    &#x2022; The right to request that we transfer or port elements of your data either to you or another service provider<br></br>
+                    &#x2022; The right to complain to your data protection regulator <br></br>
+                    </p>
+                    
+                  <p className='p1'>We may provide third parties with aggregated but anonymised information and analytics about our customers and, before we do so, we will make sure that it does not identify you.</p>
               </div>
             </div>
           </div>
