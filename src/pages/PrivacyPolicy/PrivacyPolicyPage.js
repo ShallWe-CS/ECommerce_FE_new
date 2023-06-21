d27b9f5eee2cd89e8242934ecd822e82da6d6862
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2023-06-21 15:36:49 +0530

    privacy page

--- src/pages/PrivacyPolicy/PrivacyPolicyPage.js
+++ src/pages/PrivacyPolicy/PrivacyPolicyPage.js
@@ -10,45 +10,43 @@ const PrivacyPolicyPage = () => {
             <div className='title-md'>
               <h2>PRIVACY POLICY</h2>
             </div>
+            <div className = 'privacycontent'>
+              <div className >
+                  <h3 className='my-3'></h3>
+                  <p>We’ll always keep your data safe and secure. So you’re clued up, here’s why we need it and how we use it.</p>
+              </div>
 
-            <div className='my-4'>
-                <h3 className='my-3'></h3>
-                <p>We’ll always keep your data safe and secure. So you’re clued up, here’s why we need it and how we use it.<br>
-                </br>To take the measurement correctly, lay down the garment on a flat surface and measure along the lines shown in the pictures below.</p>
-            </div>
+              <div>
+                <img src='' alt=''/>
+              </div>
 
-            <div>
-              <img src='' alt=''/>
-            </div>
+              <div>
+                <h3 className='my-3'>1. THE GOOD STUFF</h3>
+                <p>We will only use your data to up your experience.</p>
+                <h3 className='my-3'>2. LOCKED DOWN</h3>
+                <p>We’ll protect your data like it’s our own.</p>
+                <h3 className='my-3'>3. STRAIGHT TALKING</h3>
+                <p>We’ll always talk your language and have your back – no-nonsense, no surprises.</p>
+                <h3 className='my-3'>4. NO SPAM</h3>
+                <p>You decide what and how you hear from us.</p>
+                <h3 className='my-3'>5. JUST THE ESSENTIALS</h3>
+                <p>Your info won’t just hang about – if we don’t need it, we’ll delete it.</p>
+                <h3 className='my-3'>PROTECTING YOUR PRIVACY</h3>
+                <p>Pissu Kanna, we are 100% committed to protecting the privacy and security of our customers and site visitors (yep, we mean you). The Pissu Kanna team members are customers themselves, of both Pissu Kanna and other internet sites – so we understand  how important your  privacy is. If you have any questions about how we Protect Your Privacy, drop us a line at hello@pissukanna.com
+                    For all our services, the data controller — the company that’s responsible for your privacy— is pissukanna.com </p>
+                <h3 className='my-3'>SHARING YOUR INFORMATION</h3>
+                <p>We do not, and will not, sell any of your personal data to any third party – including your name, address, email address or credit card information. We want to earn and maintain your trust, and we believe this is absolutely essential in order do that.
 
-            <div>
-              <h3>T-shirts</h3>
-              <table>
-                <tr>
-                  <th>T-shirt Size</th>
-                  <th>Collor (in)</th>
-                </tr>
-                <tr>
-                  <th>S</th>
-                  <th>15</th>
-                </tr>
-                <tr>
-                  <th>M</th>
-                  <th>15.5</th>
-                </tr>
-                <tr>
-                  <th>L</th>
-                  <th>16</th>
-                </tr>
-                <tr>
-                  <th>XL</th>
-                  <th>16.5</th>
-                </tr>
-                <tr>
-                  <th>XXL</th>
-                  <th>17</th>
-                </tr>
-              </table>
+  However, we share your data with the following categories of companies as an essential part of being able to provide our services to you, as set out in this statement:
+
+  Companies in the Pissu Kanna group, as sometimes different bits of our group are responsible for different activities (especially licensed ones)
+  Companies that do things to get your purchases to you, such as payment service providers, warehouses, order packers, and delivery companies
+  Professional service providers, such as marketing agencies, advertising partners and website hosts, who help us run our business
+  Credit reference agencies, law enforcement and fraud prevention agencies, so we can help tackle fraud
+  Companies approved by you, such as social media sites (if you choose to link your accounts to us).
+  We may provide third parties with aggregated but anonymised information and analytics about our customers and, before we do so, we will make sure that it does not identify you.</p>
+
+              </div>
             </div>
           </div>
         </div>
